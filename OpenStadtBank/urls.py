bfe4baee55a76ea06442c7c86364c6090dee80ef
Ömer R. Sak <orsmes6145@gmail.com>
2026-01-29 23:09:50 +0100

    docs(license): add license mention header to source files
    Add license notices to all source files.
    
    Refs #1

--- OpenStadtBank/urls.py
+++ OpenStadtBank/urls.py
@@ -1,3 +1,6 @@
+# Copyright (C) 2026 Ömer Rasim Sak
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
 """
 URL configuration for OpenStadtBank project.
 
--- manage.py
+++ manage.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+# Copyright (C) 2026 Ömer Rasim Sak
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
